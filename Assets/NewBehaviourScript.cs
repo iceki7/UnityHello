629360ea70b164909aa4aa3fbaf3b2f626bbd07f
lceki <1300953421@qq.com>
2021-09-22 16:10:18 +0800

    20210922

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+			//transform.Rotate(new Vector3(0,RotationY,0));
+		
+	}
+	/*
+	GUI文字
+	void OnGUI()
+	{
+		GUI.skin.label.fontSize=120;
+		GUI.Label(new Rect(10,50,900,120),"Hello");
+	}*/
+
+}
---
+++ Assets/player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class player : MonoBehaviour {
+	float speed=0.1f;
+	float speed_r=2;
+	CharacterController cc;
+	Vector3 v_cam;
+	// Use this for initialization
+	void Start () {
+		cc=GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float rh=Input.GetAxis("Mouse X");
+		float rv=Input.GetAxis("Mouse Y");
+		v_cam.x-=rv*speed_r;
+		v_cam.y+=rh*speed_r;
+
+		Camera.main.transform.eulerAngles=v_cam;
+		if(Input.GetKeyDown(KeyCode.A))
+	cc.Move(this.transform.TransformDirection(new Vector3(0,0,speed)));	
+	}
+}
